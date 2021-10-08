4b04ac134abc6c6e9c3fbfcd20d6afc4ea538d3f
caternuson <caternuson@gmail.com>
2021-10-08 08:42:51 -0700

    add code

---
+++ jacobs_ladder/jacobs_ladder.py
@@ -0,0 +1,72 @@
+# simple Jacob's Ladder simulator
+# written a long time ago by T.C. Broonsie
+import time
+import random
+import displayio
+from adafruit_matrixportal.matrix import Matrix
+
+#--| User Config |-----------------------------------------
+MATRIX_WIDTH = 64
+MATRIX_HEIGHT = 32
+BACK_COLOR = 0x000000
+BOLT_COLOR = 0xFFFFFF
+BOLT_WIDTH = 5
+DELAY = 0.05
+#----------------------------------------------------------
+
+# create matrix
+matrix = Matrix(width=MATRIX_WIDTH, height=MATRIX_HEIGHT, bit_depth=6)
+display = matrix.display
+
+# create group
+splash = displayio.Group(max_size=1)
+display.show(splash)
+
+# create bitmap
+bitmap = displayio.Bitmap(MATRIX_WIDTH, MATRIX_HEIGHT, 2)
+palette = displayio.Palette(2)
+palette[0] = BACK_COLOR
+palette[1] = BOLT_COLOR
+
+# create tile grid
+tg = displayio.TileGrid(bitmap, pixel_shader=palette)
+splash.append(tg)
+
+def draw_bolt(location):
+    '''Draw random zig-zag bolt across display.'''
+    x = location
+    if random.choice([True, False]):
+        # left to right
+        y_range = range(0, MATRIX_HEIGHT)
+    else:
+        # right to left
+        y_range = range(MATRIX_HEIGHT - 1, -1, -1)
+    for y in y_range:
+        try:
+            bitmap[x, y] = 1
+        except:
+            # just don't draw it
+            pass
+        dx = x - location
+        if abs(dx) < BOLT_WIDTH:
+            # if we have room, move random direction
+            x += random.choice([-1,1])
+        else:
+            # otherwise, force back toward center
+            x += 1 if dx < 0 else -1
+
+# loop forever
+while True:
+    # start at bottom
+    xpos = 0
+    # loop until we receach the top
+    while xpos < MATRIX_WIDTH:
+        # update display
+        display.auto_refresh = False
+        bitmap.fill(0)
+        draw_bolt(xpos)
+        display.auto_refresh = True
+        # move up random amount
+        xpos += random.randrange(BOLT_WIDTH)
+        # zzzzzzz
+        time.sleep(DELAY)
---
+++ matrix_rain/matrix_rain.py
@@ -0,0 +1,114 @@
+import time
+import random
+import displayio
+import adafruit_imageload
+from adafruit_matrixportal.matrix import Matrix
+
+#--| User Config |-----------------------------------------
+# matrix setup
+MATRIX_WIDTH = 64
+MATRIX_HEIGHT = 32
+# sprite setup
+SPRITE_FILE = 'rain_sprite.bmp'
+SPRITE_WIDTH = 2
+SPRITE_HEIGHT = 3
+# animation setup
+DROP_COLOR = 0xADAF00
+TAIL_COLOR = 0x00FF00
+MAX_DROPS = 40
+TAIL_SIZE = 10
+FALL_RATE = 0.01
+MAX_STEPS = 5
+GAMMA = 0.1
+#----------------------------------------------------------
+
+FADE_STEPS = SPRITE_HEIGHT * TAIL_SIZE
+
+# create matrix
+matrix = Matrix(width=MATRIX_WIDTH, height=MATRIX_HEIGHT, bit_depth=6)
+display = matrix.display
+
+# create groups
+drops = displayio.Group(max_size=MAX_DROPS)            # these fall and change sprite
+tails = displayio.Group(max_size=MAX_DROPS*TAIL_SIZE)  # these fade and disappear
+main_group = displayio.Group(max_size=2)               # holds the drops and tails
+main_group.append(tails)
+main_group.append(drops)
+display.show(main_group)
+
+# load sprite sheet
+sprite_sheet, _ = adafruit_imageload.load(SPRITE_FILE)
+SPRITE_COUNT = sprite_sheet.width // SPRITE_WIDTH * sprite_sheet.height // SPRITE_HEIGHT
+
+# palette for drop
+drop_palette = displayio.Palette(2)
+drop_palette[0] = 0x000000
+drop_palette[1] = DROP_COLOR
+drop_palette.make_transparent(0)
+
+# palettes for fading tails
+tail_palette = []
+r = TAIL_COLOR >> 16 & 0xFF
+g = TAIL_COLOR >> 8 & 0xFF
+b = TAIL_COLOR & 0xFF
+for fade_step in range(FADE_STEPS):
+    palette = displayio.Palette(2)
+    palette[0] = 0x000000
+    palette[1] = (round(r * (1 - (fade_step/FADE_STEPS)**GAMMA)),
+                  round(g * (1 - (fade_step/FADE_STEPS)**GAMMA)),
+                  round(b * (1 - (fade_step/FADE_STEPS)**GAMMA)))
+    palette.make_transparent(0)
+    tail_palette.append(palette)
+
+def make_sprite(tile=random.randrange(SPRITE_COUNT), palette=drop_palette, x=0, y=0):
+    '''Return a sprite from the sprite sheet.'''
+    return displayio.TileGrid(sprite_sheet,
+                            pixel_shader=palette,
+                            width = 1,
+                            height = 1,
+                            tile_width = SPRITE_WIDTH,
+                            tile_height = SPRITE_HEIGHT,
+                            default_tile = tile,
+                            x = x,
+                            y = y)
+
+def update_drops():
+    '''Move all the drop sprites.'''
+    for drop in drops:
+        drop.y += 1
+        # if off screen, destroy
+        if drop.y > MATRIX_HEIGHT + SPRITE_HEIGHT:
+            drops.remove(drop)
+            continue
+        # if moved SPRITE_HEIGHT, add to tail
+        if not drop.y % SPRITE_HEIGHT:
+            tails.append(make_sprite(drop[0], tail_palette[0], drop.x, drop.y))
+        # change the sprite
+        drop[0] = random.randrange(SPRITE_COUNT)
+
+def update_tails():
+    '''Fade all the tail sprites.'''
+    for tail in tails:
+        current = tail_palette.index(tail.pixel_shader)
+        # if done fading, destroy
+        if current == FADE_STEPS - 1:
+            tails.remove(tail)
+            continue
+        # next fade step
+        tail.pixel_shader = tail_palette[current+1]
+
+#====================
+# MAIN LOOP
+#====================
+while True:
+    # if there's room, add new drop
+    if len(drops) < MAX_DROPS:
+        new_drop = make_sprite(y=-SPRITE_HEIGHT, x=random.randrange(MATRIX_WIDTH))
+        drops.append(new_drop)
+    # fall a random number of steps
+    for _ in range(random.randrange(MAX_STEPS)):
+        display.auto_refresh = False
+        update_drops()
+        update_tails()
+        display.auto_refresh = True
+        time.sleep(FALL_RATE)
